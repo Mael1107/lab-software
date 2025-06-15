2cc6f3bf6ad34960d110e770044f4eed55d2e0a7
Gabriel Silva <gabrielvsilva29022008@gmail.com>
2025-06-15 18:47:37 -0300

    Cors configured

--- backend/app/models/account.rb
+++ backend/app/models/account.rb
@@ -1,2 +1,5 @@
 class Account < ApplicationRecord
+  validates %i[ name password email cpf is_super ], presence: true
+  validates :email, email: true
+  validates :password, confirmation: true
 end
