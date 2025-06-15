45cb4c11b8cee822b86f2e8a88a6ec96084abd83
Gabriel Silva <gabrielvsilva29022008@gmail.com>
2025-06-15 20:58:07 -0300

    Restarted API

--- backend/app/models/account.rb
+++
@@ -1,5 +0,0 @@
-class Account < ApplicationRecord
-  validates %i[ name password email cpf is_super ], presence: true
-  validates :email, email: true
-  validates :password, confirmation: true
-end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,13 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_15_204434) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_15_235525) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "accounts", force: :cascade do |t|
     t.string "name"
-    t.string "password"
+    t.string "email"
+    t.string "cpf"
+    t.string "number"
+    t.boolean "is_super"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
     t.string "email"
     t.string "cpf"
     t.string "number"
